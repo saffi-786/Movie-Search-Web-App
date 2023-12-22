27119853c9c775928d4f9eceab5663e40bc58095
Saffi <sksaffiulla786@gmail.com>
2023-12-22 10:54:56 +0530

    Added title to the website

--- src/App.jsx
+++ src/App.jsx
@@ -33,12 +33,21 @@ function App() {
 
   return (
     <div className="max-w-[1200px] min-h-[400px] shadow-xl mx-auto mt-5 p-3 rounded-lg">
+      <div className="text-center">
+        <h1 className="font-bold text-4xl text-yellow-500">
+          Movies
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-red-500 to-violet-500">
+            FLIX
+          </span>
+        </h1>
+      </div>
       <div>
         <input
+          placeholder="Search"
           type="search"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="w-full rounded-md p-3 text-slate-700 shadow-md"
+          className="w-full rounded-full py-3 px-5 text-slate-700 shadow-md font-serif"
         />
       </div>
       <div className="w-full flex flex-wrap gap-4">
